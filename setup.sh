14b2726c70eb071e58bd19f0e1cc3e672dbfc7e5
air[GOD] <djlc@airGOD.local>
2017-01-22 02:13:20 +0900

    [mod]

--- setup.sh
+++ setup.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-DOT_FILES=(.bashrc .bash_profile .zshrc .vimrc .dein.toml .gvimrc .latexmkrc .template_tex)
+DOT_FILES=(.bashrc .bash_profile .zshrc .vimrc .dein.toml .gvimrc .latexmkrc .header.h .template_tex)
 
 for file in ${DOT_FILES[@]}
 do
 	ln -s $HOME/dotfiles/$file $HOME/$file
 done
+
+if [ ! -e .header.h.pch ]; then
+    clang++ -std=gnu++1y -x c++-header .header.h -o .header.h.pch
+fi
+
