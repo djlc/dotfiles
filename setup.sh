88f9a859989072102ecbd604f88b72c5c764f493
djlc <cdn60127@gmail.com>
2017-03-06 07:04:51 +0900

    add git settings

--- setup.sh
+++ setup.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
+# symbolic links setting
 DOT_FILES=(.bashrc .bash_profile .zshrc .vimrc .dein.toml .gvimrc .latexmkrc .header.h .template_tex)
-
 for file in ${DOT_FILES[@]}
 do
 	ln -s $HOME/dotfiles/$file $HOME/$file
 done
 
+# create clang pch file
 if [ ! -e .header.h.pch ]; then
     clang++ -std=gnu++1y -x c++-header .header.h -o .header.h.pch
 fi
+
+# git
+git config --global user.name "djlc"
+git config --global user.email "cdn60127@gmail.com"
+git config --global core.editor 'vim -c "set fenc=utf-8'
+git config --global color.diff auto
+git config --global color.status auto
+git config --global color.branch auto
+git config --global core.precomposeunicode true
+git config --global core.quotepath true
 
